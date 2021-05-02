40a5c623be19671b81a5db07caddb677fcceec1e
Maxime Boyer <75278784+Max13crypt@users.noreply.github.com>
2021-05-02 13:54:08 +0200

    ok

---
+++ app/src/main/java/com/example/lychar_fenetre_glissante/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.example.lychar_fenetre_glissante.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
